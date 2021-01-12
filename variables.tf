bbc64e4618e3e6a6c2af48d558ac847b17b41cfb
Armando Martinez <armando.martinez@truss.works>
2021-01-12 12:04:23 -0800

    fixing formatting

--- variables.tf
+++ variables.tf
@@ -3,7 +3,6 @@ variable "region" {
   type        = string
   default     = "us-west-2"
 }
-
 variable "azs" {
   description = "List of AZs to manage using only the letters, not full AZ name"
   type        = list
